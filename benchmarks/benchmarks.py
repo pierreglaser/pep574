c17d14b357c106e440fa68bfe7791dccf6534eb6
Pierre Glaser <pierreglaser@msn.com>
2018-09-21 18:08:31 +0200

    use out-of-ram pickling with dump and load
    +minor style fix

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,6 +3,8 @@
 #
 # author: Pierre Glaser
 """bechmarks for no-copy pickling of numpy arrays"""
+import gc
+import os
 import pickle
 
 import numpy as np
@@ -14,37 +16,111 @@ class Benchmark:
     number = 1
     timeout = 30
     params = [
-        ((100, 100), (1000, 100), (1000, 1000), (10000, 1000)),
+        ((10000, 1000), (10000, 10000)),
     ]
     param_names = ['shape']
+    temp_filename = 'tmp_array.pkl'
 
     def setup(self, shape):
         self.data = np.random.randn(*shape)
 
 
-class Protocol5WithBuffer(Benchmark):
+class Protocol5WithOutOfBandBuffer(Benchmark):
     def peakmem_pickle(self, shape):
         buffers = []
-        stream = pickle.dumps(
-            self.data, protocol=5, buffer_callback=buffers.append)
-        b = pickle.loads(stream, buffers=buffers)
+        try:
+
+            with open(self.temp_filename, 'wb') as f:
+                pickle.dump(
+                    self.data, f, protocol=5, buffer_callback=buffers.append)
+
+            with open(self.temp_filename, 'rb') as f:
+                self.data = pickle.load(f, buffers=buffers)
+
+        finally:
+            os.unlink(self.temp_filename)
+
     peakmem_pickle.benchmark_name = 'peakmem_protocol_5_pickling_with_buffers'
+
+    def time_pickle(self, shape):
+        buffers = []
+        try:
+
+            with open(self.temp_filename, 'wb') as f:
+                pickle.dump(
+                    self.data, f, protocol=5, buffer_callback=buffers.append)
+
+            with open(self.temp_filename, 'rb') as f:
+                self.data = pickle.load(f, buffers=buffers)
+
+        finally:
+            os.unlink(self.temp_filename)
+
+    time_pickle.benchmark_name = 'time_protocol_5_pickling_with_buffers'
 
 
 class Protocol4(Benchmark):
-    def setup(self, shape):
-        self.data = np.random.randn(*shape)
-
     def peakmem_pickle(self, shape):
         buffers = []
-        stream = pickle.dumps(self.data, protocol=4)
-        b = pickle.loads(stream)
+        try:
+
+            with open(self.temp_filename, 'wb') as f:
+                pickle.dump(
+                    self.data, f, protocol=4)
+
+            with open(self.temp_filename, 'rb') as f:
+                self.data = pickle.load(f)
+
+        finally:
+            os.unlink(self.temp_filename)
+
     peakmem_pickle.benchmark_name = 'peakmem_protocol_4_pickling'
+
+    def time_pickle(self, shape):
+        buffers = []
+        try:
+
+            with open(self.temp_filename, 'wb') as f:
+                pickle.dump(
+                    self.data, f, protocol=4)
+
+            with open(self.temp_filename, 'rb') as f:
+                self.data = pickle.load(f)
+
+        finally:
+            os.unlink(self.temp_filename)
+
+    time_pickle.benchmark_name = 'time_protocol_4_pickling'
 
 
 class Protocol5(Benchmark):
     def peakmem_pickle(self, shape):
         buffers = []
-        stream = pickle.dumps(self.data, protocol=5)
-        b = pickle.loads(stream)
+        try:
+
+            with open(self.temp_filename, 'wb') as f:
+                pickle.dump(
+                    self.data, f, protocol=5)
+
+            with open(self.temp_filename, 'rb') as f:
+                self.data = pickle.load(f)
+
+        finally:
+            os.unlink(self.temp_filename)
+
     peakmem_pickle.benchmark_name = 'peakmem_protocol_5_pickling'
+
+    def time_pickle(self, shape):
+        buffers = []
+        try:
+
+            with open(self.temp_filename, 'wb') as f:
+                pickle.dump(
+                    self.data, f, protocol=5)
+
+            with open(self.temp_filename, 'rb') as f:
+                self.data = pickle.load(f)
+
+        finally:
+            os.unlink(self.temp_filename)
+    time_pickle.benchmark_name = 'time_protocol_5_pickling'
