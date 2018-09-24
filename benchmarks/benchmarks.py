b46bb5b29289b6b063074434f9acdcb4eebda7b0
Pierre Glaser <pierreglaser@msn.com>
2018-09-24 18:23:36 +0200

    updated test to take in account the pickle5 backport

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -5,7 +5,7 @@
 """bechmarks for no-copy pickling of numpy arrays"""
 import gc
 import os
-import pickle
+import sys
 
 import numpy as np
 
@@ -16,10 +16,14 @@ class Benchmark:
     number = 1
     timeout = 30
     params = [
-        ((10000, 1000), (10000, 10000)),
+        (
+            # (10000, 1000),
+            (10000, 10000),
+        ),
     ]
     param_names = ['shape']
     temp_filename = 'tmp_array.pkl'
+    processes = 2
 
     def setup(self, shape):
         self.data = np.random.randn(*shape)
@@ -27,6 +31,12 @@ class Benchmark:
 
 class Protocol5WithOutOfBandBuffer(Benchmark):
     def peakmem_pickle(self, shape):
+        # In <3.8 pythons, the pickle module does not provide protocol 5
+        # support, so we use the pickle5 backport instead
+        if sys.version < '3.8':
+            import pickle5 as pickle
+        else:
+            import pickle
         buffers = []
         try:
 
@@ -40,9 +50,14 @@ class Protocol5WithOutOfBandBuffer(Benchmark):
         finally:
             os.unlink(self.temp_filename)
 
-    peakmem_pickle.benchmark_name = 'peakmem_protocol_5_pickling_with_buffers'
+    peakmem_pickle.benchmark_name = (
+        'peakmem_protocol_5_pickling_with_out_of_band_buffers')
 
     def time_pickle(self, shape):
+        if sys.version < '3.8':
+            import pickle5 as pickle
+        else:
+            import pickle
         buffers = []
         try:
 
@@ -56,17 +71,19 @@ class Protocol5WithOutOfBandBuffer(Benchmark):
         finally:
             os.unlink(self.temp_filename)
 
-    time_pickle.benchmark_name = 'time_protocol_5_pickling_with_buffers'
+    time_pickle.benchmark_name = (
+        'time_protocol_5_pickling_with_out_of_band_buffers')
 
 
 class Protocol4(Benchmark):
     def peakmem_pickle(self, shape):
+        import pickle
+
         buffers = []
         try:
 
             with open(self.temp_filename, 'wb') as f:
-                pickle.dump(
-                    self.data, f, protocol=4)
+                pickle.dump(self.data, f, protocol=4)
 
             with open(self.temp_filename, 'rb') as f:
                 self.data = pickle.load(f)
@@ -77,12 +94,12 @@ class Protocol4(Benchmark):
     peakmem_pickle.benchmark_name = 'peakmem_protocol_4_pickling'
 
     def time_pickle(self, shape):
+        import pickle
         buffers = []
         try:
 
             with open(self.temp_filename, 'wb') as f:
-                pickle.dump(
-                    self.data, f, protocol=4)
+                pickle.dump(self.data, f, protocol=4)
 
             with open(self.temp_filename, 'rb') as f:
                 self.data = pickle.load(f)
@@ -93,14 +110,18 @@ class Protocol4(Benchmark):
     time_pickle.benchmark_name = 'time_protocol_4_pickling'
 
 
-class Protocol5(Benchmark):
+class Protocol5NoOutOfBandBuffer(Benchmark):
     def peakmem_pickle(self, shape):
+        if sys.version < '3.8':
+            import pickle5 as pickle
+        else:
+            import pickle
+
         buffers = []
         try:
 
             with open(self.temp_filename, 'wb') as f:
-                pickle.dump(
-                    self.data, f, protocol=5)
+                pickle.dump(self.data, f, protocol=5)
 
             with open(self.temp_filename, 'rb') as f:
                 self.data = pickle.load(f)
@@ -108,19 +129,37 @@ class Protocol5(Benchmark):
         finally:
             os.unlink(self.temp_filename)
 
-    peakmem_pickle.benchmark_name = 'peakmem_protocol_5_pickling'
+    peakmem_pickle.benchmark_name = (
+        'peakmem_protocol_5_pickling_no_out_of_band_buffer')
 
     def time_pickle(self, shape):
+        if sys.version < '3.8':
+            import pickle5 as pickle
+        else:
+            import pickle
         buffers = []
         try:
 
             with open(self.temp_filename, 'wb') as f:
-                pickle.dump(
-                    self.data, f, protocol=5)
+                pickle.dump(self.data, f, protocol=5)
 
             with open(self.temp_filename, 'rb') as f:
                 self.data = pickle.load(f)
 
         finally:
             os.unlink(self.temp_filename)
-    time_pickle.benchmark_name = 'time_protocol_5_pickling'
+
+    time_pickle.benchmark_name = (
+        'time_protocol_5_pickling_no_out_of_band_buffer')
+
+
+# class ArrayReduceEx(Benchmark):
+#     def peakmem_reduce_ex(self, shape):
+#         _ = self.data.__reduce_ex__(5)
+
+#     peakmem_reduce_ex.benchmark_name = 'peakmem_reduce_ex'
+
+#     def peakmem_reduce(self, shape):
+#         _ = self.data.__reduce__()
+
+#     peakmem_reduce.benchmark_name = 'peakmem_reduce'
